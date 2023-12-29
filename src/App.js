883990d56433856b3b95c71a7a794d3553ad7f16
Wis3477 <mabohlalenkuna@gmail.com>
2023-12-29 04:49:44 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar"; 
-
+import Form from "./components/Form";
 function App() {
   return (
     <div>
       <Navbar />
       <Sidebar />
+      <Form />
+     
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,28 @@
+import "./Sidebar.css";
 function Sidebar() {
     return (
-        <h1>SIDEBAR</h1>
+        <div className="sidebar">
+        <div className="sidebar-item active-item">
+          <span className="material-icons-outlined hover active">lightbulb</span>
+          <span className="sidebar-text">Notes</span>
+        </div>
+        <div className="sidebar-item">
+          <span className="material-icons-outlined hover">notifications</span>
+          <span className="sidebar-text">Reminders</span>
+        </div>
+        <div className="sidebar-item">
+          <span className="material-icons-outlined hover">edit</span>
+          <span className="sidebar-text">Edit Labels</span>
+        </div>
+        <div className="sidebar-item">
+          <span className="material-icons-outlined hover">archive</span>
+          <span className="sidebar-text">Archive</span>
+        </div>
+        <div className="sidebar-item">
+          <span className="material-icons-outlined hover">delete</span>
+          <span className="sidebar-text">Trash</span>
+        </div>
+      </div>
     )
 }
 
